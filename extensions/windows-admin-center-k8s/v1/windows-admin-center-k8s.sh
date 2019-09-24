1454136e613918bff24370428fab230861eec5dc
Alexander Boswell <alboswel@microsoft.com>
2019-09-24 15:56:05 -0700

    update intial example

--- extensions/windows-admin-center-k8s/v1/windows-admin-center-k8s.sh
+++ extensions/windows-admin-center-k8s/v1/windows-admin-center-k8s.sh
@@ -36,7 +36,7 @@ fi
 # Deploy container
 echo $(date) " - Deploying hello-world container"
 
-kubectl run hello-world --quiet --image=busybox --restart=OnFailure -- echo "Hello Kubernetes!"
+kubectl run hello-world --quiet --image=busybox --restart=OnFailure -- echo "Hello from Windows Admin Center!"
 
 echo $(date) " - run kubectl get pods --show-all to list the pods"
 echo $(date) " - run kubectl logs (passing the pod name gathered from kubectl get pods)"
